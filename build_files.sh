107f4c661765549e911b1cff5551cb7f2d74515e
Zhiyuan Rong <zhiyuar2@uci.edu>
2026-04-10 04:00:17 -0700

    Allow Vercel build dependency install

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python3 -m pip install -r requirements.txt
+python3 -m pip install --break-system-packages -r requirements.txt
 python3 manage.py collectstatic --no-input
